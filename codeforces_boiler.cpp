cde78ab039dd0d602a0eee7d3677fc25afded0e9
AmberLehmann <132320992+AmberLehmann@users.noreply.github.com>
2024-04-14 13:32:16 -0400

    update: boiler

--- codeforces_boiler.cpp
+++ codeforces_boiler.cpp
@@ -1,17 +1,49 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-#define send {ios_base::sync_with_stdio(false);}
-#define help {cin.tie(NULL);}
-
+#define uwu {ios_base::sync_with_stdio(false);}
+#define owo {cin.tie(NULL);}
+#define all(x) (x).begin(), (x).end();
+typedef long long int ll;
+/*⠀⠀⠀⠀⠀⠀⡤⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⠞⠀⢀⣳⠔⠊⢉⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⡎⢀⠔⠋⠀⠀⣠⣎⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⡸⢸⠾⣅⡀⠐⡄⠈⠁⠀⠀⢀⡼⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⠗⠓⠢⡄⠙⡆⠘⢀⣠⠴⠚⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠤⠤⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⡠⠋⠀⠀⠀⢸⣀⡷⠚⠉⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠖⠋⠁⠀⠀⠀⣸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⡰⠁⠀⠀⠀⣠⡞⢹⡆⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⣠⠖⠉⠀⠀⠀⠀⠀⠀⣰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢠⡇⠀⠀⣠⠊⠀⡇⠸⡇⠀⢀⡇⠀⠀⣀⠀⠀⠀⢠⠞⠁⠀⣀⡀⠀⠀⠀⢀⣼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠈⡇⠀⢰⢻⠀⠀⡇⢰⡇⠀⣸⠀⣰⠋⠈⢳⡀⡴⠃⢀⡴⠊⢡⠃⠀⠀⡰⢫⠏⠀⠀⠀⠀⠀⠀⠀⢀⣠⠤⠒⠉⣩⠇⠀
+⠀⠈⠦⣝⠘⡆⠀⢇⢸⠁⢠⠟⢶⠃⠀⠀⡀⢿⠁⡰⠋⠀⡰⠃⠀⠀⢋⡴⠃⠀⠀⠀⠀⠀⢀⣴⠚⠯⡀⠀⠀⠈⢉⣩⠇
+⠀⠀⠀⢿⡛⠻⣄⡘⡏⠀⣾⣀⡎⠀⠀⡸⠀⠘⡼⠁⣠⣾⠥⠤⠤⢶⡋⠀⠀⠀⠀⠀⡠⠞⠁⠈⠙⡆⢹⣠⠴⠊⠉⠀⠀
+⠀⠀⠀⢠⡵⢤⠀⠙⠂⠈⠁⠀⢇⠀⠀⢇⠀⠀⠛⠉⠁⣸⠀⠀⠀⢸⠃⠀⠀⢀⡤⠊⠀⠀⠀⠀⢀⡿⠚⠁⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢩⣯⠖⢋⠑⠲⣄⠀⠘⠦⠤⠼⠚⣿⣀⣠⠔⠁⠀⠀⣠⠏⢀⣠⠔⠋⠀⠀⠀⠀⣠⠖⠻⡄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⡞⢁⠞⢩⠙⡆⠘⡆⢀⡴⠊⣉⠙⢾⣀⣀⣀⡠⠴⠞⠓⠒⠉⠀⠀⠀⠀⢀⡤⠞⢹⠀⠀⢳⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢰⠃⢸⠀⠸⢤⡇⠀⣱⠋⣰⠛⡏⢣⠘⡆⠈⠓⠦⢄⣀⣀⣀⣀⣀⡠⠴⠚⠁⠀⠀⡜⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠸⡄⠈⠓⠒⠊⠀⠠⠤⠀⢧⠒⠁⡼⢠⠃⠀⠀⠀⠀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⣰⠃⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠙⣦⠤⣄⠀⠳⢤⡠⠴⢪⡑⠋⢀⡼⠀⢀⡠⠒⠉⠀⠀⢀⣉⡲⣄⠀⠀⠀⡴⠃⠀⠀⢀⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⡇⠀⡌⠳⣵⠋⣇⠀⢠⡷⠚⠉⢀⠔⠋⠀⠀⣠⠔⠫⣅⠀⠈⠈⢳⣤⠞⠁⢀⣀⡤⣾⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢧⠀⢱⡀⡁⢀⡇⠀⡼⣙⡦⠞⠁⠀⠀⢠⣞⣁⡤⠤⢬⣳⣠⠴⠊⠀⠀⠒⠉⣠⠔⠃⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⡼⠣⣀⡽⠛⢯⣀⠾⣉⡁⠀⠀⣀⡠⠞⠁⠀⠀⠀⠀⠀⠀⠉⠢⡤⠤⢶⠒⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⡞⠀⢠⢿⠀⠀⠁⠀⠀⠀⠈⠉⠉⠀⠀⠀⠀⠀⠀⠀⣰⠂⠀⠀⠀⠘⢆⠸⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⡼⠀⠀⡞⢸⠀⠀⠀⠀⢠⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠃⠀⠀⠀⠀⠀⠘⣆⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣇⠀⠀⣷⡘⡆⠱⣄⠀⡞⠀⠀⠀⠀⢀⠀⡀⠀⠀⣀⣘⠀⠀⠀⠀⠀⠀⠀⠸⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠘⣄⠀⢹⡉⠻⡄⠈⢲⠁⠀⠀⠀⢀⣎⡤⠤⠔⢶⠤⠌⣧⡀⠀⠀⠀⠀⠀⠀⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⠓⠤⣑⡄⠱⡄⡏⠀⠀⠀⣠⠏⠁⠀⠀⠀⠈⠣⡀⠀⠈⠓⣤⣀⠀⠀⠀⠈⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⣻⠁⠀⢀⡜⢹⡄⠀⠀⠀⠀⠀⠀⠙⢦⡀⠀⢸⡈⠑⡆⠀⠀⡗⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⣀⣀⣀⣠⠇⠀⢠⠎⠀⣸⠁⠀⠀⠀⠀⠀⠀⠀⢀⡇⢀⣼⠁⢀⡇⠀⣰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠣⣀⠀⡾⣄⢴⠯⢤⡴⡇⠀⠀⠀⠀⠀⠀⠀⠀⡼⠛⢱⠇⠀⢸⠷⢋⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠠⣞⣉⣁⡼⠁⢡⣿⣶⠋⢰⡇⠀⠀⠀⠀⠀⠀⢀⡴⠃⢠⠏⠀⣰⠇⠀⡾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣠⢋⡀⢀⡞⢸⣳⣠⠞⠀⠀⠀⠀⠀⠀⠀⠸⠼⠵⠋⠀⣾⢇⣆⡼⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠻⠯⠴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀*/
 const string PAIR_LEFT = "(";
 const string PAIR_RIGHT = ")";
 
-const string IT_LEFT = "";
-const string IT_RIGHT = "";
+// const string IT_LEFT = "";
+// const string IT_RIGHT = "";
 
-// const string IT_LEFT = "[";
-// const string IT_RIGHT = "]";
+const string IT_LEFT = "[";
+const string IT_RIGHT = "]";
 
 // const string PAIR_LEFT = "{";
 // const string PAIR_RIGHT = "}";
@@ -36,6 +68,12 @@ template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p
     return cin >> p.second;
 }
 
+//Automatically input a vector given size
+template<typename T>
+istream& operator >>(istream &cin, vector<T> &v) {
+  for (auto &x: v) cin >> x;
+  return cin;
+}
 template<typename T> void debug(string s, T x) {cerr << "\033[1;34m" << s << "\033[0;32m = \033[35m" << x << "\033[0m\n";}
 template<typename T, typename... Args> void debug(string s, T x, Args... args) {for (int i=0, b=0; i<(int)s.size(); i++) if (s[i] == '(' || s[i] == '{') b++; else
         if (s[i] == ')' || s[i] == '}') b--; else if (s[i] == ',' && b == 0) {cerr << "\033[1;34m" << s.substr(0, i) << "\033[0;32m = \033[35m" << x << "\033[31m | "; debug(s.substr(s.find_first_not_of(' ', i + 1)), args...); break;}}
@@ -52,23 +90,23 @@ const bool local_ = false;
 #endif */
 
 const bool run = local_ ? 0 : 1;
-void solve(int tc = 0) {
+void solve(int T = 0) {
 
 } 
 
 int main() {
         auto begin = std::chrono::high_resolution_clock::now();
     
-    send help
+    uwu owo
 
     cout << setprecision(15) << fixed;
     cerr << setprecision(4) << fixed;
             
-    int tc = 1;
+    int T = 1;
     // if (local_)
     // if (!run)
-    cin >> tc;
-    for (int t = 0; t < tc; t++) {
+    cin >> T;
+    for (int t = 0; t < T; t++) {
         pr(t); prs(string(50, '-'));
         solve(t);
         prs(string(50, '-') + "\n");
---
+++ problemset/NextRound.cpp
@@ -0,0 +1,93 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+#define im {ios_base::sync_with_stdio(false);}
+#define gay {cin.tie(NULL);}
+
+const string PAIR_LEFT = "(";
+const string PAIR_RIGHT = ")";
+
+const string IT_LEFT = "";
+const string IT_RIGHT = "";
+
+// const string IT_LEFT = "[";
+// const string IT_RIGHT = "]";
+
+// const string PAIR_LEFT = "{";
+// const string PAIR_RIGHT = "}";
+// const string IT_LEFT = "{";
+// const string IT_RIGHT = "}";
+
+// benq - print any container + pair
+template<typename T, typename = void> struct is_iterable : false_type {};
+template<typename T> struct is_iterable<T, void_t<decltype(begin(declval<T>())),decltype(end(declval<T>()))>> : true_type {};
+template<typename T> typename enable_if<is_iterable<T>::value&&!is_same<T, string>::value,ostream&>::type operator<<(ostream &cout, T const &v);
+template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p) { return cout << PAIR_LEFT << p.f << ", " << p.s << PAIR_RIGHT; }
+template<typename T> typename enable_if<is_iterable<T>::value&&!is_same<T, string>::value,ostream&>::type operator<<(ostream &cout, T const &v) {
+    cout << IT_LEFT; 
+    for (auto it = v.begin(); it != v.end();) {
+        cout << *it;
+        if (++it != v.end()) cout << ", ";
+    }
+    return cout << IT_RIGHT;
+}
+template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p) {
+    cin >> p.first;
+    return cin >> p.second;
+}
+template<typename T>
+istream& operator >>(istream &cin, vector<T> &v) {
+  for (auto &x: v) cin >> x;
+  return cin;
+}
+template<typename T> void debug(string s, T x) {cerr << "\033[1;34m" << s << "\033[0;32m = \033[35m" << x << "\033[0m\n";}
+template<typename T, typename... Args> void debug(string s, T x, Args... args) {for (int i=0, b=0; i<(int)s.size(); i++) if (s[i] == '(' || s[i] == '{') b++; else
+        if (s[i] == ')' || s[i] == '}') b--; else if (s[i] == ',' && b == 0) {cerr << "\033[1;34m" << s.substr(0, i) << "\033[0;32m = \033[35m" << x << "\033[31m | "; debug(s.substr(s.find_first_not_of(' ', i + 1)), args...); break;}}
+template<typename T> void debug_nameless(T x) {cerr << "\033[35m" << x << "\033[0m\n";}
+template<typename T, typename... Args> void debug_nameless(T x, Args... args) {cerr << "\033[35m" << x << "\033[31m | "; debug_nameless(args...);}
+
+#define pr(...) debug(#__VA_ARGS__, __VA_ARGS__)
+#define prs(...) debug_nameless(__VA_ARGS__)
+const bool local_ = true;
+/* #else
+#define pr(...) 135
+#define prs(...) 135
+const bool local_ = false;
+#endif */
+
+const bool run = local_ ? 0 : 1;
+void solve(int tc = 0) {
+  int n, k, c = 0;
+  cin >> n >> k;
+  vector<int> scores(n);
+  cin >> scores;
+  for(int i = 0; i < n; i++) {
+    if(scores.at(i) >= scores.at(k-1) && scores.at(i) != 0)
+      c++;
+  }
+  cout << c << endl;
+} 
+
+int main() {
+        auto begin = std::chrono::high_resolution_clock::now();
+    
+    im gay
+
+    cout << setprecision(15) << fixed;
+    cerr << setprecision(4) << fixed;
+            
+    int tc = 1;
+    // if (local_)
+    // if (!run)
+    // cin >> tc;
+    for (int t = 0; t < tc; t++) {
+        pr(t); prs(string(50, '-'));
+        solve(t);
+        prs(string(50, '-') + "\n");
+    }
+    
+        auto end = std::chrono::high_resolution_clock::now();
+        cerr << setprecision(4) << fixed;
+        cerr << "Execution time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin).count() << " seconds" << endl;
+}
